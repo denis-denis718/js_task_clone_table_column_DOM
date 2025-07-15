8d112b029caf4b234644dc467b9f4a87b9b8d2ba
Denis <denchick1546450@gmail.com>
2025-07-16 00:15:08 +0300

    add final solution(fix length)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,14 @@
 const rows = document.querySelectorAll('table tr');
 
 rows.forEach((row) => {
+  const cells = row.children;
 
-    const cells = row.children;
+  if (cells.lenght < 2) {
+    return;
+  } // check: had "Position" or not
 
-    if(cells.lenght < 2) return; // check: had "Position" or not
+  const positionCell = cells[1]; // second column - 'Position'
+  const cloned = positionCell.cloneNode(true); // clone her
 
-    const positionCell = cells[1]; // second column - 'Position'
-    const cloned = positionCell.cloneNode(true); // clone her 
-
-    row.insertBefore(cloned, cells[cells.length - 1]); // paste before salary 
+  row.insertBefore(cloned, cells[cells.length - 1]); // paste before salary
 });
